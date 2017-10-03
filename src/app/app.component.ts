6a13490bb68e88d858960bd808c7eb22326fcdb7
Dplaza <dplaza@equilibrha.es>
2017-10-03 15:40:49 +0200

    Dia 3 sesion 1 completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'Agenda';
+  
+  contactos: string[] = [
+    'Tim Cook',
+    'Bill Gates',
+    'Elon Musk',
+    'Steve Wozniak',
+    'Sundar Pichai'
+  ];
+
+  ngOnInit(){
+
+    console.log('estoy vivo')
+
+  }
+
 }
